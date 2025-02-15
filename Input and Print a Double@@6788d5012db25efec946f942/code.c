31db68a12588525b2a743f64b8ecc4b96c1a51a8
kanishka-aro05 <193149393+kanishka-aro05@users.noreply.github.com>
2025-02-15 16:01:47 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,11 @@
+// Your code here...#include <stdio.h>
+
+int main() {
+    float x;
+
+    scanf("%.4f",x);  // Read two words (separated by space)
+
+    printf("You entered: %f\n", x);  // Print the words
+
+    return 0;
+}
